c60f33e81a8374ffd739dbd79d17b43145a04ba7
MiladinKi <miladinkovacevic12@gmail.com>
2023-10-09 17:53:15 +0200

    Initial commit

---
+++ src/test/java/com/iktpreobuka/e_dnevnik/ProjectEDnevnikApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iktpreobuka.e_dnevnik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectEDnevnikApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
